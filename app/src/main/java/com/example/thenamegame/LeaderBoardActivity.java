67677078906211d35efbbc6e79ce70ba11fad6a1
IanOldsUMD <olds0024@umn.edu>
2021-04-27 16:47:53 -0500

    added LeaderBoardActivity

---
+++ app/src/main/java/com/example/thenamegame/LeaderBoardActivity.java
@@ -0,0 +1,9 @@
+package com.example.thenamegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * this class displays the leaderboards
+ */
+public class LeaderBoardActivity extends AppCompatActivity {
+}
