667545958b7422317eedc494b7e91b9d4a7b94eb
Patrick Sowada <sowad025@d.umn.com>
2021-04-27 16:43:42 -0500

    renamed PlayActivity and fleshed out ideal functionality in comments

--- app/src/main/java/com/example/thenamegame/PlayActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.thenamegame;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-/**
- * THis menu lets the user decide what to play challenge/nameGame/Stars
- */
-public class PlayActivity extends AppCompatActivity {
-}
