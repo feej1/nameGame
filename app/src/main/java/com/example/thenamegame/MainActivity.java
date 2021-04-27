62323beaa11d6ad9e3b48e67acf7cacab89d09fb
IanOldsUMD <olds0024@umn.edu>
2021-04-27 16:57:33 -0500

    added Intent switching with main menu buttons

--- app/src/main/java/com/example/thenamegame/MainActivity.java
+++ app/src/main/java/com/example/thenamegame/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.thenamegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    /**
+     * @param view
+     * @author Ian Olds
+     */
+    public void switchToFormType(View view){
+        Intent intent = new Intent(this, FormType.class);
+        startActivity(intent);
+    }
+
+    /**
+     * @param view
+     * @author Ian Olds
+     */
+    public void switchToLeaderBoard(View view){
+        Intent intent = new Intent(this, LeaderBoardActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * @param view
+     * @author Ian Olds
+     */
+    public void switchToGoogleLogin(View view){
+        Intent intent = new Intent(this, GoogleLoginActivity.class);
+        startActivity(intent);
     }
 }
