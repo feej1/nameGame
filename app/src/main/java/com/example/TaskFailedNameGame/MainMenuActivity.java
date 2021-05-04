caf6f9d365ef0282a3a3d97e3c392b2bc0c30187
buchl017 <buchl017@d.umn.edu>
2021-05-04 17:48:20 -0500

    added the rest of the google sign in button event stuff

--- app/src/main/java/com/example/TaskFailedNameGame/MainMenuActivity.java
+++ app/src/main/java/com/example/TaskFailedNameGame/MainMenuActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.TaskFailedNameGame.Retro.RetroDataRunner;
@@ -12,6 +13,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,7 +23,9 @@ import com.google.firebase.auth.FirebaseUser;
  */
 public class MainMenuActivity extends AppCompatActivity {
 
-//    FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 00000;
+    //    FirebaseAuth mAuth;
+GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -47,7 +52,7 @@ public class MainMenuActivity extends AppCompatActivity {
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
         // Initialize Firebase Auth
@@ -64,7 +69,41 @@ public class MainMenuActivity extends AppCompatActivity {
 //        updateUI(account);
     }
 
+    public void googleOnclick(View v){
+        signIn();
+    }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
+    }
 
     public void switchToFormType(View view){
         Intent intent = new Intent(this, FormTypeActivity.class);
