2e962865ee82e94d75e8900784691bc70d83263d
buchl017 <buchl017@d.umn.edu>
2021-05-04 17:43:46 -0500

    adding google sign in

--- app/src/main/java/com/example/TaskFailedNameGame/MainMenuActivity.java
+++ app/src/main/java/com/example/TaskFailedNameGame/MainMenuActivity.java
@@ -8,11 +8,19 @@ import android.view.View;
 
 import com.example.TaskFailedNameGame.Retro.RetroDataRunner;
 import com.example.TaskFailedNameGame.Retro.RetroQuestionRunner;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * @author Ian
  */
 public class MainMenuActivity extends AppCompatActivity {
+
+//    FirebaseAuth mAuth;
 
 
     @Override
@@ -30,7 +38,32 @@ public class MainMenuActivity extends AppCompatActivity {
         // starts all retro runners
         RetroQuestionRunner.getOneNameInstance(0).start();
         RetroQuestionRunner.getOneYearInstance(0).start();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("484220769255-eqreisk19guhm0bjasg8ks2adcarscfn.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        // Initialize Firebase Auth
+//        mAuth = FirebaseAuth.getInstance();
+
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        updateUI(account);
+    }
+
 
 
     public void switchToFormType(View view){
@@ -46,7 +79,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
 
     public void switchToGoogleLogin(View view){
-        Intent intent = new Intent(this, GoogleLoginActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, GoogleLoginActivity.class);
+//        startActivity(intent);
     }
 }
