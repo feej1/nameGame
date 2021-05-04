ac9b1632a4598d460e16e0ebcd473e703e2f9bbb
buchl017 <buchl017@d.umn.edu>
2021-05-04 09:38:37 -0500

    adding google signin options

--- app/src/main/java/com/example/thenamegame/GoogleLoginActivity.java
+++ app/src/main/java/com/example/thenamegame/GoogleLoginActivity.java
@@ -1,9 +1,27 @@
 package com.example.thenamegame;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 /**
- * Lets the user login
+ * Lets the user login with Google account
  */
 public class GoogleLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestedIdToken()
+                .requestEmail()
+                .build();
+
+
+    }
 }
